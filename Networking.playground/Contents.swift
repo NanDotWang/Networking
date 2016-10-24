9072dcd8267299f3b6f42c4759135bef4182ecbb
Nan Wang <Nan.Wang@UNIBET.com>
2016-10-24 15:20:48 +0200

    Cleanup code

--- Networking.playground/Contents.swift
+++ Networking.playground/Contents.swift
@@ -1,13 +1,12 @@
 import Foundation
 import PlaygroundSupport
-
-/// Need this to get networking request callbacks
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 /*
+ * Example 1
+ *
  * Make a single networking request and get model back
  */
-
 //ProfileRequest(name: "onevcat").send() { (result) in
 //    switch result {
 //    case .empty:
@@ -20,19 +19,24 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 //}
 
 /*
- * Get an AsyncOperation (with / without completetion handler) from the request
+ * Example 2
+ *
+ * Queue a request into an OperationQueue
  */
-let profileRequestOperation = ProfileRequest(name: "onevcat").asyncOperation() //{ (result) in
-//    switch result {
-//    case .empty:
-//        print("show empty state")
-//    case let .error(error):
-//        print("show error state with: \(error)")
-//    case let .success(profile):
-//        print("show success: \(profile)")
-//    }
-//}
 
+/// Get an asyncOperation that does a request
+let profileRequestOperation = ProfileRequest(name: "onevcat").asyncOperation() { (result) in
+    switch result {
+    case .empty:
+        print("show empty state")
+    case let .error(error):
+        print("show error state with: \(error)")
+    case let .success(profile):
+        print("show success: \(profile)")
+    }
+}
+
+/// AysncBlock operation count down 1
 let one = AsyncBlockOperation { (finish) in
     DispatchQueue.global().asyncAfter(deadline: .now() + 3) {
         print("One.")
@@ -40,6 +44,7 @@ let one = AsyncBlockOperation { (finish) in
     }
 }
 
+/// AysncBlock operation count down 2
 let two = AsyncBlockOperation { (finish) in
     DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
         print("Two.")
@@ -47,6 +52,7 @@ let two = AsyncBlockOperation { (finish) in
     }
 }
 
+/// AysncBlock operation count down 3
 let three = AsyncBlockOperation { (finish) in
     DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
         print("Three.")
@@ -54,13 +60,13 @@ let three = AsyncBlockOperation { (finish) in
     }
 }
 
+/// initialize a OperationQueue
 let queue = OperationQueue()
 
+/// setup dependencies
 two.addDependency(three)
-
 one.addDependency(two)
-
 profileRequestOperation.addDependency(one)
 
+/// queue up operations
 queue.addOperations([one, two, three, profileRequestOperation], waitUntilFinished: false)
-
